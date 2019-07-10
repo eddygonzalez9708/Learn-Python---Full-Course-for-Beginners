8547e5f1647509291dbf998a13c224e4bc705303
Eddy <edward.gonzalez9708@gmail.com>
2019-07-10 12:18:49 -0400

    Completed the second hour of the tutorial.

---
+++ list_functions.py
@@ -0,0 +1,8 @@
+lucky_numbers = [5, 4, 8, 15, 16, 23, 42]
+friends = ["Kevin", "Karen", "Jim", "Jim", "Oscar", "Toby"]
+
+friends2 = friends.copy()
+friends2[1] = "Mike"
+
+print(friends)
+print(friends2)
---
+++ return_statement.py
@@ -0,0 +1,5 @@
+def cube(num):
+  return num**3
+
+result = cube(4)
+print(result)
